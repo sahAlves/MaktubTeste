356db8114b8fca888ff7a42e8e12f33a3207836a
sahAlves <sah.alves002@gmail.com>
2020-06-22 22:15:08 -0300

    Finalizado

--- banco.sql
+++ banco.sql
@@ -31,7 +31,7 @@ CREATE TABLE `tbl_contato` (
   `telefone_celular` varchar(30) NOT NULL,
   `observacao` varchar(1000) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,6 @@ CREATE TABLE `tbl_contato` (
 
 LOCK TABLES `tbl_contato` WRITE;
 /*!40000 ALTER TABLE `tbl_contato` DISABLE KEYS */;
-INSERT INTO `tbl_contato` VALUES (1,'Sônia Maria Ferreira','sonia@ferreira.com','(11) 4773-6517','lalallalala');
 /*!40000 ALTER TABLE `tbl_contato` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -67,34 +66,6 @@ LOCK TABLES `tbl_faixa_etaria` WRITE;
 /*!40000 ALTER TABLE `tbl_faixa_etaria` DISABLE KEYS */;
 INSERT INTO `tbl_faixa_etaria` VALUES (1,'0-18',20),(2,'19-23',30),(3,'24-28',40),(4,'29-33',50),(5,'34-38',60),(6,'39-43',70),(7,'44-48',80),(8,'49-53',90),(9,'54-58',100),(10,'59 +',110);
 /*!40000 ALTER TABLE `tbl_faixa_etaria` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tbl_faixaetaria_simulacao`
---
-
-DROP TABLE IF EXISTS `tbl_faixaetaria_simulacao`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tbl_faixaetaria_simulacao` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_faixa_etaria` int(11) DEFAULT NULL,
-  `id_simulacao` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_faixa_etaria_idx` (`id_faixa_etaria`),
-  KEY `id_simulacao_idx` (`id_simulacao`),
-  CONSTRAINT `id_faixa_etaria` FOREIGN KEY (`id_faixa_etaria`) REFERENCES `tbl_faixa_etaria` (`id`),
-  CONSTRAINT `id_simulacao` FOREIGN KEY (`id_simulacao`) REFERENCES `tbl_simulacao` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tbl_faixaetaria_simulacao`
---
-
-LOCK TABLES `tbl_faixaetaria_simulacao` WRITE;
-/*!40000 ALTER TABLE `tbl_faixaetaria_simulacao` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tbl_faixaetaria_simulacao` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -138,7 +109,7 @@ CREATE TABLE `tbl_simulacao` (
   `modalidade` varchar(200) NOT NULL,
   `preco` double NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -147,7 +118,6 @@ CREATE TABLE `tbl_simulacao` (
 
 LOCK TABLES `tbl_simulacao` WRITE;
 /*!40000 ALTER TABLE `tbl_simulacao` DISABLE KEYS */;
-INSERT INTO `tbl_simulacao` VALUES (1,'Sabrina','(11) 4773-6517','Amil','Enfermaria',420.8),(2,'Sônia Maria Ferreira','(11) 4773-6517','Amil','Apartamento',570.9),(3,'Sabrina','1111111','Amil','Apartamento',570.9),(4,'Silva','(11) 4773-6517','Amil','Apartamento',630.9),(5,'Sônia Maria Ferreira','(11) 4773-6517','Amil','Enfermaria',380.8),(6,'Sabrina','(11) 4773-6517','Amil','Apartamento',570.9),(7,'Sabrina','(1','Amil','Apartamento',580.9),(8,'Sabrina','(11) 4773-6517','Bradesco Saúde','Apartamento',780.9),(9,'Sasah','(11) 4773-6517','NotreDame Intermédica','Apartamento',740.9),(10,'Sônia Maria Ferreira','(11) 4773-6517','Unimed','Apartamento',640.9),(11,'Sônia Maria Ferreira','(11) 4773-6517','Unimed','Apartamento',640.9),(12,'Davi','(11) 4773-6517','NotreDame Intermédica','Apartamento',680.9),(13,'Sasah','(11) 4773-6517','Amil','Apartamento',580.9),(14,'Sabrina','(11) 4773-6517','Amil','Apartamento',580.9),(15,'Sabrina','(11) 4773-6517','NotreDame Intermédica','Apartamento',670.9),(16,'sa','(11) 4773-6517','Unimed','Apartamento',550.9);
 /*!40000 ALTER TABLE `tbl_simulacao` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -185,4 +155,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-22  6:32:50
+-- Dump completed on 2020-06-22 22:13:02
